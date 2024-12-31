ad9109da3f818d272e8d7a3d635de139b2a4ead0
Lucas C. Noman <lucas.noman@outlook.com>
2024-12-31 16:53:39 -0300

    refactor(Caso de uso e design patterns): Inversão de dependências

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,31 +1,27 @@
 import { hash } from 'bcryptjs'
 
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
-
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
 }
 
-export async function registerUseCase({
-  email,
-  name,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
 
-    const prismaUsersRepository = new PrismaUsersRepository()
+  async execute({ email, name, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prismaUsersRepository.findByEmail(email)
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
   }
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
