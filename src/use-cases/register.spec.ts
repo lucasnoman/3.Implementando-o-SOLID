35c9de3526a27a241d787ec3a8a6ddacb74c4004
Lucas C. Noman <lucas.noman@outlook.com>
2025-01-02 10:08:31 -0300

    test(Design Patterns & Testes): Primeiro teste unitário

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,37 @@
-import { expect, test } from 'vitest'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+import { RegisterUseCase } from '@/use-cases/register'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail() {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'Zé do teste',
+      email: 'zedoteste@example.com',
+      password: '1a2b3c4d',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '1a2b3c4d',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
