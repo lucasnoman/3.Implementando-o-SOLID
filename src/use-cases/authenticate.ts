24b9d0c75c3dd5ec2eb6f449ce04d8a7568ced12
Lucas C. Noman <lucas.noman@outlook.com>
2025-01-03 11:44:53 -0300

    feat(Implementando casos de uso): Caso de uso de autenticação

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,37 @@
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+
+import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    password,
+    email,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
