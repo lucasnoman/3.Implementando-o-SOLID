1fcb3a26d0c5483d2f7d4c643ac1defcb5ecb4f9
Lucas C. Noman <lucas.noman@outlook.com>
2025-01-05 22:08:23 -0300

    feat(Implementando casos de uso): Caso de uso de métricas

---
+++ src/use-cases/get-users-metrics.spec.ts
@@ -0,0 +1,32 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { GetUserMetricsUseCase } from '@/use-cases/get-users-metrics'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: GetUserMetricsUseCase
+
+describe('Get Users Metrics Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('should be able to get chck-ins count from metrics', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+
+    const { checkInsCount } = await sut.execute({
+      userId: 'user-01',
+    })
+
+    expect(checkInsCount).toEqual(2)
+  })
+})
