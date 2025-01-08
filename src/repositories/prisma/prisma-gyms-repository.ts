8136e1743206d7776798e5ff0b6ca077d35a6b60
Lucas C. Noman <lucas.noman@outlook.com>
2025-01-08 14:20:56 -0300

    feat(Controllers & Testes E2E): Repositório de academias

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,43 @@
+import { Gym, Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
+
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    return await prisma.gym.create({
+      data,
+    })
+  }
+
+  async searchMany(query: string, page: number) {
+    return await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+  }
+
+  async findById(id: string) {
+    return await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    return await prisma.$queryRaw<Gym[]>`
+        SELECT *
+        from gyms
+        WHERE (6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) *
+                           cos(radians(longitude) - radians(${longitude})) +
+                           sin(radians(${latitude})) * sin(radians(latitude)))) < 10
+    `
+  }
+}
