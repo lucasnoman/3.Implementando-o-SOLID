0e143269f2a7e2048bdb035309b829abb50c7433
Lucas C. Noman <lucas.noman@outlook.com>
2025-01-09 10:30:42 -0300

    feat(Controllers & Testes E2E): Outros controllers da academia

---
+++ src/http/controller/gyms/nearby.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => Math.abs(value) <= 90),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180),
+  })
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.query)
+
+  const fetchNearbyGymUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymUseCase.execute({
+    userLongitude: longitude,
+    userLatitude: latitude,
+  })
+
+  return reply.status(201).send({ gyms })
+}
--- src/http/controller/gyms/routes.ts
+++ src/http/controller/gyms/routes.ts
@@ -2,7 +2,16 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middleware/verify-jwt'
 
+import { create } from './create'
+import { nearby } from './nearby'
+import { search } from './search'
+
 export async function gymsRoutes(app: FastifyInstance) {
   // Todas as rotas abaixo desse addHook chamarão o middleware que verifica autenticação
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.get('/gyms', create)
 }
