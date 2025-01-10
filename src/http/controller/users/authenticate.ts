73de96017e2a1cd9317707a99b457b36f8ec217d
Lucas C. Noman <lucas.noman@outlook.com>
2025-01-10 09:02:43 -0300

    refactor(Controllers & Testes E2E): Implementação do refresh token

--- src/http/controller/users/authenticate.ts
+++ src/http/controller/users/authenticate.ts
@@ -42,7 +42,7 @@ export async function authenticate(
     return reply
       .setCookie('refreshToken', refreshToken, {
         path: '/',
-        secure: true,
+        // secure: true,
         sameSite: true,
         httpOnly: true,
       })
